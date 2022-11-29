f2671b0e05d1643a38b50381ff1480434d298bfe
Denis McMahon <denismcmahon@Deniss-MacBook-Pro.local>
2022-11-29 11:16:54 +0000

    Basic components started

--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -34,5 +34,7 @@ export class TokenStorageService {
     if(user) {
       return JSON.parse(user);
     }
+
+    return {};
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form: any = {
+    user: null,
+    email: null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
+  constructor(private authService: AuthService) { }
+
+  ngOnInit():void {
+
+  }
+
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    )
+  }
 }
